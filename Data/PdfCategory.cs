c2377332bd0eab96be41df4169f7e4c5c3fb2aaf
felixyangyicheng <admin-fr06ab@ermo-group.tosb>
2023-04-27 15:58:06 +0200

    upload table content

---
+++ Data/PdfCategory.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace judo_univ_rennes.Data
+{
+    public enum PdfCategory
+    {
+        [Display(Name = "Technique")]
+        [Description("Technique")]
+        Techniques,
+        [Display(Name = "Géneral")]
+        [Description("Géneral")]
+
+        General,
+        [Display(Name = "Techniques de bras")]
+        [Description("Techniques de bras")]
+
+        Bras,
+        [Display(Name = "Techniques de hanche")]
+        [Description("Techniques de hanche")]
+
+        Hanche,
+        [Display(Name = "Techniques de Jambe")]
+        [Description("Techniques de Jambe")]
+
+        Jambe,
+
+
+    }
+
+
+}
---
+++ Static/EnumExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+namespace judo_univ_rennes.Static
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+              .GetMember(enumValue.ToString())
+              .First()
+              .GetCustomAttribute<DisplayAttribute>()
+              ?.GetName();
+        }
+
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
+        where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
+        }
+
+        public static string StringValueOfEnum(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            else
+            {
+                return value.ToString();
+            }
+        }
+    }
+}
