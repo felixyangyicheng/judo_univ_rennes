26093a32da04126266f2cd2feec06b0c7e340cf3
felixyangyicheng <admin-fr06ab@ermo-group.tosb>
2023-04-28 10:24:37 +0200

    pdf api controller

---
+++ Controllers/PdfController.cs
@@ -0,0 +1,48 @@
+﻿using judo_univ_rennes.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace judo_univ_rennes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        private readonly ILogger<PdfController> logger;
+        private readonly IPdfRepo _pdfRepo;
+        public PdfController(ILogger<PdfController> logger, IPdfRepo pdfRepo)
+        {
+            this.logger = logger;
+            this._pdfRepo = pdfRepo;
+        }
+        /// <summary>
+        /// Download pdf
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        [HttpGet]
+
+        [Route("{filename}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> GetOne( string filename)
+        {
+            logger.LogInformation($"Pdf Attempt GET {filename}");
+
+            try
+            {
+                var db = _pdfRepo.GetAsync(filename).Result;
+                return File(db.Content, "application/pdf");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Something Went Wrong in the {nameof(GetOne)}");
+                return Problem($"Something Went Wrong in the {nameof(GetOne)}", statusCode: 500);
+            }
+        }
+    }
+}
