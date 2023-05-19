897d674829e234863c7ac891e4d9ca56df19ba30
felixyangyicheng <admin-fr06ab@ermo-group.tosb>
2023-05-19 15:06:35 +0200

    pdf view new tab

---
+++ Components/Tabs/PdfTab.razor.cs
@@ -0,0 +1,45 @@
+﻿using Blazored.LocalStorage;
+using judo_univ_rennes.Contracts;
+using judo_univ_rennes.Data;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace judo_univ_rennes.Components.Tabs
+{
+    public partial class PdfTab
+    {
+        #region Properties
+
+        [Parameter] public string fileName { get; set; }
+
+        [Inject] IConfiguration _config { get; set; }
+        [Inject] IPdfRepo _pdfRepo { get; set; }
+        [Inject] ILocalStorageService _localStorage { get; set; }
+        private bool loading { get; set; } = false;
+
+        private string pdfLink { get; set; } = "";
+        public PdfModel pdf { get; set; }
+
+
+        #endregion
+        #region methods
+        protected override async Task OnParametersSetAsync()
+        {
+            loading = true;
+            pdf = await _pdfRepo.GetByNameAsync(fileName);
+            if (pdf != null)
+            {
+                var getBase = _config.GetSection("BaseAddress").GetRequiredSection("dev");
+                string protocole = getBase.GetRequiredSection("Protocole").Value;
+                string host = getBase.GetRequiredSection("Host").Value;
+                string port = getBase.GetRequiredSection("Port").Value;
+                pdfLink = $"{protocole}{host}{port}/api/pdf/";
+            }
+            await base.OnParametersSetAsync();
+            loading = false;
+        }
+
+
+        #endregion
+    }
+}
