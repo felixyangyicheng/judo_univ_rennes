a2903092dbeb78c7fac67dbbdae329b4254ddb4a
felixyangyicheng <felix.yang.yicheng@gmail.com>
2023-08-10 22:28:15 +0200

    markup string html content for richtext

--- Data/BaseTextItem.cs
+++ Data/BaseTextItem.cs
@@ -1,9 +1,13 @@
-﻿namespace judo_univ_rennes.Data
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace judo_univ_rennes.Data
 {
     public class BaseTextItem
     {
         public int Id { get; set; }
-        public int Content { get; set; }
+        [Column("Content", TypeName = "text")]
+        public string Content { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
         public virtual ApiUser ApiUser { get; set; }
--- Pages/ClientViews/ReportBug.razor.cs
+++ Pages/ClientViews/ReportBug.razor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Blazorise.RichTextEdit;
+using Microsoft.AspNetCore.Components;
 
 namespace judo_univ_rennes.Pages.ClientViews
 {
@@ -7,16 +8,27 @@ namespace judo_univ_rennes.Pages.ClientViews
 	{
         protected RichTextEdit richTextEditRef;
         protected bool readOnly;
-        protected string contentAsHtml;
+        protected string contentAsHtml=" Signaler un bug...";
+
+        protected MarkupString DisplayContent;
         protected string contentAsDeltaJson;
         protected string contentAsText;
         protected string savedContent;
 
+
+        protected override Task OnInitializedAsync( )
+        {
+            DisplayContent = (MarkupString)contentAsHtml;
+
+            return base.OnInitializedAsync();
+        }
         public async Task OnContentChanged()
         {
             contentAsHtml = await richTextEditRef.GetHtmlAsync();
+            DisplayContent =(MarkupString) contentAsHtml;
             contentAsDeltaJson = await richTextEditRef.GetDeltaAsync();
             contentAsText = await richTextEditRef.GetTextAsync();
+            StateHasChanged();
         }
 
         public async Task OnSave()
