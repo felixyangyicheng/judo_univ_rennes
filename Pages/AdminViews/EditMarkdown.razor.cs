6c8c8daf6d64b514c64b148e596bcef3d8526632
felixyangyicheng <felix.yang.yicheng@gmail.com>
2023-08-21 09:35:26 +0200

    footer style

--- Pages/AdminViews/EditMarkdown.razor.cs
+++ Pages/AdminViews/EditMarkdown.razor.cs
@@ -7,7 +7,7 @@ namespace judo_univ_rennes.Pages.AdminViews
 {
 	public  partial class EditMarkdown
 	{
-        protected string markdownValue = "# EasyMDE \n Go ahead, play around with the editor! Be sure to check out **bold**, *italic*";
+        protected string markdownValue = "## Table\n| Right | Left | Default | Center |\n|------:|:-----|---------|:------:|\n|   12  |  12  |    12   |    12  |\n|  123  |  123 |   123   |   123  |\n|    1  |    1 |     1   |     1  |\n+---------------+---------------+--------------------+\n| Fruit         | Price         | Advantages         |\n+===============+===============+====================+\n| Bananas       | $1.34         | - built-in wrapper |\n|               |               | - bright color     |\n+---------------+---------------+--------------------+\n| Oranges       | $2.10         | - cures scurvy     |\n|               |               | - tasty            |\n+---------------+---------------+--------------------+\n## Header Identifiers in HTML\n[Header identifiers in HTML](#header-identifiers-in-html-given)";
 
         protected string markdownHtml;
         protected MarkupString DisplayContent;
